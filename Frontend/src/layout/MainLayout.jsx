e9c650c7b383e2b24956c0bcf1fc3b2c4b384f6e
ahasan2912 <habib15-4284@diu.edu.bd>
2025-05-16 09:49:05 +0600

    Develop Navbar Section from developer ahasan

--- Frontend/src/layout/MainLayout.jsx
+++ Frontend/src/layout/MainLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
     return (
         <div>
             <div>
-                <Outlet></Outlet>
+                <Navbar/>
+            </div>
+            <div>
+                <Outlet/>
             </div>
         </div>
     );
